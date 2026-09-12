632767c94cb9d96d2dbaf6fa07e02639eaf2d290
Utkarsh-2577 <utkarshranjan2577@gmail.com>
2026-09-12 17:49:36 +0530

    LC-path-sum [Runtime: 0 ms; Memory: 21.6 MB]

--- tree/path-sum.cpp
+++ tree/path-sum.cpp
@@ -11,23 +11,23 @@
  */
 class Solution {
 public:
-    bool bfs(TreeNode* root, int sum, int target){
+    bool dfs(TreeNode* root, int sum, int target){
         if(!root->left && !root->right){
             if(sum == target) return true;
             return false;
         }
 
         if(root->left){
-            if(bfs(root->left, sum+root->left->val, target)) return true;
+            if(dfs(root->left, sum+root->left->val, target)) return true;
         }
         if(root->right){
-            if(bfs(root->right, sum+root->right->val, target)) return true;
+            if(dfs(root->right, sum+root->right->val, target)) return true;
         }
 
         return false;
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(!root) return false;
-        return bfs(root, root->val, targetSum);
+        return dfs(root, root->val, targetSum);
     }
 };
